c8e5a0e69c20e0d05d1a2a924d057d39e72a0fdf
Leonel Amaya <omar.mejia2099@gmail.com>
2026-02-06 00:03:03 -0500

    chore: initialize database and jobs table with python script

---
+++ app/db/init_db.py
@@ -0,0 +1,55 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+
+def init_database():
+    conn = mysql.connector.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+    cursor = conn.cursor()
+
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}")
+    cursor.close()
+    conn.close()
+
+    conn = mysql.connector.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    )
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS jobs (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        status VARCHAR(20) NOT NULL,
+        fecha_inicial DATE NOT NULL,
+        fecha_final DATE NOT NULL,
+        limit_rows INT NOT NULL,
+        started_at DATETIME NULL,
+        finished_at DATETIME NULL,
+        error_message TEXT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    cursor.close()
+    conn.close()
+
+    print("DB creada")
+
+if __name__ == "__main__":
+    init_database()
